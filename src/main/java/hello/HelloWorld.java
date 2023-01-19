95eac46a7b18094fd7e9123acea46aec11b77236
root <root@ip-172-31-35-219.ap-south-1.compute.internal>
2023-01-19 05:42:31 +0000

    Added Few Files

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.joda.time.LocalTime;
+import static com.rollbar.notifier.config.ConfigBuilder.withAccessToken;
+import com.rollbar.notifier.Rollbar;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+      LocalTime currentTime = new LocalTime();
+		  System.out.println("The current local time is: " + currentTime);
+
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+	    
+    Rollbar rollbar = Rollbar.init(withAccessToken("f14a1ad6e0e644009d456897cfab76ac").build());
+    rollbar.log("Hello, Rollbar");
+    rollbar.close(true);
+	    
+    }
+}
